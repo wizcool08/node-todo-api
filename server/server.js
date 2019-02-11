d296562217da19c08ea294683f57decd6a0d9cfd
Kim Yong <kimyong.lim.2017@gmail.com>
2019-02-11 15:56:54 +0800

    Get /todos/:id endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -35,6 +36,26 @@ app.get("/todos", (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+// GET /todos/123456
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
